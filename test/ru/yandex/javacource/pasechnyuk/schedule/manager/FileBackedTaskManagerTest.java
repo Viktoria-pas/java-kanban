dbbe10aca028b5354dc6966c6d4e10106cc4fee9
Viktoria-pas <vpasechnyuk@yandex.com>
2025-03-07 14:41:12 +0100

    ФЗ по 8 спринту Добалены новые поля, работа со временем , новые тесты

--- test/ru/yandex/javacource/pasechnyuk/schedule/manager/FileBackedTaskManagerTest.java
+++ test/ru/yandex/javacource/pasechnyuk/schedule/manager/FileBackedTaskManagerTest.java
@@ -1,5 +1,6 @@
 package ru.yandex.javacource.pasechnyuk.schedule.manager;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.yandex.javacource.pasechnyuk.schedule.task.Epic;
 import ru.yandex.javacource.pasechnyuk.schedule.task.Subtask;
@@ -10,11 +11,26 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.time.Duration;
+import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class FileBackedTaskManagerTest {
+class FileBackedTaskManagerTest extends TaskManagerTest<FileBackedTaskManager> {
 
+    private File tempFile;
+
+    @BeforeEach
+    public void setUp() throws IOException {
+        tempFile = File.createTempFile("tasks", ".csv");
+        tempFile.deleteOnExit();
+        taskManager = createTaskManager();
+    }
+
+    @Override
+    protected FileBackedTaskManager createTaskManager() {
+        return new FileBackedTaskManager(tempFile);
+    }
 
     @Test
     public void testSaveAndLoadEmptyFile() throws IOException {
@@ -35,9 +51,11 @@ class FileBackedTaskManagerTest {
         File file = File.createTempFile("test", ".csv");
 
         FileBackedTaskManager manager = new FileBackedTaskManager(file);
-        Task task = manager.createTask(new Task("Task 1", "Description 1", 1));
+        Task task = manager.createTask(new Task("Task 1", "Description 1", 1,
+                LocalDateTime.of(2025, 5, 2, 10, 0), Duration.ofMinutes(34)));
         Epic epic = manager.createEpic(new Epic("Epic 1", "Description Epic 1", 2));
-        Subtask subtask = manager.createSubtask(new Subtask("Subtask 1", "Description Subtask 1", 3, epic.getId()));
+        Subtask subtask = manager.createSubtask(new Subtask("Subtask 1", "Description Subtask 1", 3, epic.getId(),
+                LocalDateTime.of(2025, 2, 16, 23, 0), Duration.ofMinutes(12)));
 
         FileBackedTaskManager manager2 = FileBackedTaskManager.loadFromFile(file);
 
@@ -51,9 +69,11 @@ class FileBackedTaskManagerTest {
         File file = File.createTempFile("test", ".csv");
 
         FileBackedTaskManager manager = new FileBackedTaskManager(file);
-        Task task = manager.createTask(new Task("Task 1", "Description 1", 1));
+        Task task = manager.createTask(new Task("Task 1", "Description 1", 1,
+                LocalDateTime.of(2025, 5, 2, 10, 0), Duration.ofMinutes(34)));
         Epic epic = manager.createEpic(new Epic("Epic 1", "Description Epic 1", 2));
-        Subtask subtask = manager.createSubtask(new Subtask("Subtask 1", "Description Subtask 1", 3, epic.getId()));
+        Subtask subtask = manager.createSubtask(new Subtask("Subtask 1", "Description Subtask 1", 3, epic.getId(),
+                LocalDateTime.of(2025, 2, 16, 23, 0), Duration.ofMinutes(12)));
 
 
         FileBackedTaskManager manager2 = FileBackedTaskManager.loadFromFile(file);
@@ -72,9 +92,11 @@ class FileBackedTaskManagerTest {
         File file = File.createTempFile("test", ".csv");
 
         FileBackedTaskManager manager = new FileBackedTaskManager(file);
-        Task task = manager.createTask(new Task("Task 1", "Description 1", 1));
+        Task task = manager.createTask(new Task("Task 1", "Description 1", 1,
+                LocalDateTime.of(2025, 5, 2, 10, 0), Duration.ofMinutes(34)));
         Epic epic = manager.createEpic(new Epic("Epic 1", "Description Epic 1", 2));
-        Subtask subtask = manager.createSubtask(new Subtask("Subtask 1", "Description Subtask 1", 3, epic.getId()));
+        Subtask subtask = manager.createSubtask(new Subtask("Subtask 1", "Description Subtask 1", 3, epic.getId(),
+                LocalDateTime.of(2025, 2, 16, 23, 0), Duration.ofMinutes(12)));
 
 
         FileBackedTaskManager manager2 = FileBackedTaskManager.loadFromFile(file);
@@ -94,22 +116,25 @@ class FileBackedTaskManagerTest {
         File file = File.createTempFile("test", ".csv");
 
         FileBackedTaskManager manager = new FileBackedTaskManager(file);
-        Task task = manager.createTask(new Task("Task 1", "Description 1", 1));
+        Task task = manager.createTask(new Task("Task 1", "Description 1", 1,
+                LocalDateTime.of(2025, 5, 2, 10, 0), Duration.ofMinutes(34)));
         Epic epic = manager.createEpic(new Epic("Epic 1", "Description Epic 1", 2));
-        Subtask subtask = manager.createSubtask(new Subtask("Subtask 1", "Description Subtask 1", 3, epic.getId()));
+        Subtask subtask = manager.createSubtask(new Subtask("Subtask 1", "Description Subtask 1", 3, epic.getId(),
+
+                LocalDateTime.of(2025, 2, 16, 23, 0), Duration.ofMinutes(12)));
 
         try (BufferedReader br = new BufferedReader(new FileReader(file, StandardCharsets.UTF_8))) {
             String header = br.readLine();
-            assertEquals("id,type,name,status,description,epic", header, "Заголовок CSV должен совпадать");
+            assertEquals("id,type,name,status,description, startTime, duration, epic", header, "Заголовок CSV должен совпадать");
 
             String taskLine = br.readLine();
-            assertEquals("1,TASK,Task 1,NEW,Description 1,", taskLine, "Строка задачи должна совпадать");
+            assertEquals("1,TASK,Task 1,NEW,Description 1,2025-05-02T10:00,34,", taskLine, "Строка задачи должна совпадать");
 
             String epicLine = br.readLine();
-            assertEquals("2,EPIC,Epic 1,NEW,Description Epic 1,", epicLine, "Строка эпика должна совпадать");
+            assertEquals("2,EPIC,Epic 1,NEW,Description Epic 1,2025-02-16T23:00,12,", epicLine, "Строка эпика должна совпадать");
 
             String subtaskLine = br.readLine();
-            assertEquals("3,SUBTASK,Subtask 1,NEW,Description Subtask 1,2", subtaskLine, "Строка подзадачи должна совпадать");
+            assertEquals("3,SUBTASK,Subtask 1,NEW,Description Subtask 1,2025-02-16T23:00,12,2", subtaskLine, "Строка подзадачи должна совпадать");
         }
 
 
--- test/ru/yandex/javacource/pasechnyuk/schedule/task/SubtaskTest.java
+++ test/ru/yandex/javacource/pasechnyuk/schedule/task/SubtaskTest.java
@@ -5,6 +5,10 @@ import org.junit.jupiter.api.Test;
 import ru.yandex.javacource.pasechnyuk.schedule.manager.Managers;
 import ru.yandex.javacource.pasechnyuk.schedule.manager.TaskManager;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SubtaskTest {
@@ -19,7 +23,8 @@ class SubtaskTest {
         epic1 = inMemoryTaskManager.createEpic(new Epic("Переезд в новый дом",
                 "Спланировать переезд в новый дом", 1));
         subtask1 = inMemoryTaskManager.createSubtask(new Subtask("Упаковка вещи",
-                "Упаковать вещи в коробки, хрупкие вещи в пленку", 5, epic1.getId()));
+                "Упаковать вещи в коробки, хрупкие вещи в пленку", 5, epic1.getId(),
+                LocalDateTime.of(2025, 5, 2, 10, 0), Duration.ofMinutes(60)));
 
     }
 
@@ -38,7 +43,8 @@ class SubtaskTest {
     @Test
     void subtaskWasCreated() {
         Subtask subtask2 = inMemoryTaskManager.createSubtask(new Subtask("Арендовать грузовик",
-                "Позвонить в транспортные компании", 2, epic1.getId()));
+                "Позвонить в транспортные компании", 2, epic1.getId(),
+                LocalDateTime.of(2025, 5, 2, 10, 0), Duration.ofMinutes(60)));
         assertNotNull(inMemoryTaskManager.getSubtask(),
                 "Список подзадач пуст");
     }
@@ -46,7 +52,8 @@ class SubtaskTest {
     @Test
     void subtaskAreEqualIfIdsAreEqual() {
         Subtask subtask2 = new Subtask("Арендовать грузовик",
-                "Позвонить в транспортные компании", 2, epic1.getId());
+                "Позвонить в транспортные компании", 2, epic1.getId(),
+                LocalDateTime.of(2025, 5, 2, 10, 0), Duration.ofMinutes(60));
         assertEquals(subtask1, subtask2, "Задачи с одинаковыми ID должны быть равны");
     }
 
@@ -81,7 +88,8 @@ class SubtaskTest {
     @Test
     void epicStatusUpdatesWithSubtasks() {
         Subtask subtask2 = inMemoryTaskManager.createSubtask(new Subtask("Арендовать грузовик",
-                "Позвонить в транспортные компании", 2, epic1.getId()));
+                "Позвонить в транспортные компании", 2, epic1.getId(),
+                LocalDateTime.of(2025, 6, 2, 10, 0), Duration.ofMinutes(60)));
 
         assertEquals(TaskStatus.NEW, epic1.getStatus(), "Статус эпика должен быть NEW");
 
@@ -102,7 +110,8 @@ class SubtaskTest {
     @Test
     void createSubtaskWithInvalidEpicId() {
         Subtask subtask3 = inMemoryTaskManager.createSubtask(new Subtask("Подзадача 3",
-                "Тест", 1, 999));
+                "Тест", 1, 999,
+                LocalDateTime.of(2025, 6, 2, 10, 0), Duration.ofMinutes(60)));
         assertNull(subtask3, "Указан неверный EPIC ID");
     }
 
@@ -110,10 +119,24 @@ class SubtaskTest {
     void subtaskCannotBeItsOwnEpic() {
         Subtask invalidSubtask = new Subtask("Некорректная подзадача",
                 "Попытка сделать её своим же эпиком",
-                1, 1);
+                1, 1,
+                LocalDateTime.of(2025, 6, 2, 10, 0), Duration.ofMinutes(60));
         Subtask result = inMemoryTaskManager.createSubtask(invalidSubtask);
 
         assertNull(result, "Подзадача, у которой epicId совпадает с её собственным id, не должна быть создана");
+    }
+
+    @Test
+    void setStartTimeTask() {
+        subtask1.setStartTime(LocalDateTime.of(2025, 9, 3, 18, 54));
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm");
+        assertEquals("03.09.2025 18:54", subtask1.getStartTime().format(formatter), "Время подзадачи не обновилось");
+    }
+
+    @Test
+    void setDurationTask() {
+        subtask1.setDuration(Duration.ofMinutes(15));
+        assertEquals(15L, subtask1.getDuration().toMinutes(), "Продолжительность подзадачи не обновилась");
     }
 
 
